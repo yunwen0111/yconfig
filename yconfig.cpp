88975e33b6c308eabc90492e2ff270dfccedbabe
Yvan <yvan@iairfly.com>
2019-03-06 16:25:55 +0800

    Clean some code.

--- yconfig.cpp
+++ yconfig.cpp
@@ -302,16 +302,13 @@ extern "C" void yconfig_destroy(yconfig_t *yc)
 
 extern "C" int yconfig_parse(yconfig_t *yc, const char *filename)
 {
-    FILE *fp = NULL;
-    char *buf = NULL;
-
-    fp = fopen(filename, "rb");
+    FILE *fp = fopen(filename, "rb");
     if (!fp)
         return 1;
 
     yc->sname.clear();
 
-    buf = (char *)malloc(LEN_MAX);
+    char *buf = (char *)malloc(LEN_MAX);
     for (int i = 1; fgets(buf, LEN_MAX, fp); ++i)
         if (line_to_tree(yc, buf))
             printf("Error: line %d, skip it.\n", i);
@@ -330,19 +327,17 @@ extern "C" int yconfig_file(yconfig_t *yc, const char *filename)
     if (!fp)
         return 1;
 
-    if (yc) {
-        for (map<string, map<string, yconfig_val_t> >::const_iterator it
-                = yc->vals.begin(); it != yc->vals.end(); ++it) {
-            fprintf(fp, "[%s]\n", it->first.c_str());
-            for (map<string, yconfig_val_t>::const_iterator it2
-                    = it->second.begin(); it2 != it->second.end(); ++it2) {
-                if (it2->second.type == YCONFIG_VALUE_TYPE_STRING)
-                        fprintf(fp, "%s = \"%s\"\n", it2->first.c_str(),
-                                (const char *)it2->second.val);
-                else if (it2->second.type == YCONFIG_VALUE_TYPE_INT)
-                        fprintf(fp, "%s = %d\n", it2->first.c_str(),
-                                *(const int *)it2->second.val);
-            }
+    for (map<string, map<string, yconfig_val_t> >::const_iterator it
+            = yc->vals.begin(); it != yc->vals.end(); ++it) {
+        fprintf(fp, "[%s]\n", it->first.c_str());
+        for (map<string, yconfig_val_t>::const_iterator it2
+                = it->second.begin(); it2 != it->second.end(); ++it2) {
+            if (it2->second.type == YCONFIG_VALUE_TYPE_STRING)
+                fprintf(fp, "%s = \"%s\"\n", it2->first.c_str(),
+                        (const char *)it2->second.val);
+            else if (it2->second.type == YCONFIG_VALUE_TYPE_INT)
+                fprintf(fp, "%s = %d\n", it2->first.c_str(),
+                        *(const int *)it2->second.val);
         }
     }
 
